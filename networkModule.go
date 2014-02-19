8591eedcf13900a3e8782e740ddeba5a02a0d5cd
cifffen <christianfsaetre@gmail.com>
2014-02-19 13:28:29 +0100

    kkkkkk

--- networkModule.go
+++ networkModule.go
@@ -8,7 +8,7 @@ import (
 )
 
 //		initializes the pc's ip adress, standard master port and the UDP broadcast connection 
-func Init()(string, string, *UDPConn, error) {
+func Init()(string, string, *net.UDPConn, error) {
 	AllAddr, err := net.InterfaceAddrs()
 	if err != nil{
 		fmt.Println("couldn't find ip")
@@ -23,7 +23,7 @@ func Init()(string, string, *UDPConn, error) {
 }
 
 //			initializes/reset state, resets/initialize number of slaves, and initialize master-ip, initialize connections array		
-func StateInit(conn *UDPConn)(string, string){
+func StateInit(conn *net.UDPConn)(string, string){
 	buffer := make([]byte,128)
 	for{ 
 		conn.SetDeadline(time.Now().Add(4*time.Second))
@@ -34,21 +34,20 @@ func StateInit(conn *UDPConn)(string, string){
 		}
 //	step) hvis man hørte en master, returner "slave" + ip adresse  
 		read_ip:=string(buffer)
-        master_ip,_:= strings.Split(read_ip, "\x00")[0]
+        master_ip:= strings.Split(read_ip, "\x00")[0]
 		return "slave", master_ip
 	}
 }
 
 //		Listens for slaves and returns the slave connection
-func MakeSlave(port string)(*TCPConn, error){
+func SlaveListener(port string)(*net.TCPListener, error){
 	listen_adress, err := net.ResolveTCPAddr("tcp", port)
 	slave_listener, err := net.ListenTCP("tcp", listen_adress)
-	conn, err := slave_listener.Accept()
-	return conn, err
+	return slave_listener, err
 }
 
 // 		attempts to connect to the given adress
-func ConnectMaster(adress string)(*TCPConn, error){
+func ConnectMaster(adress string)(*net.TCPConn, error){
 	master_adr, err :=  net.ResolveTCPAddr("tcp", adress)
 	conn, err := net.DialTCP("tcp", nil, master_adr)
 	return conn, err 
@@ -61,35 +60,44 @@ func ConnectMaster(adress string)(*TCPConn, error){
 func NetworkModule(){
 //udefinert state loop,	Lurt å ha heis funksjon sammen med ip?, bare ha ting i init som man er SIKKER på at kun skal kjøres EN gang?
 	ip, master_port, broadcast_conn, err := Init()
+	fmt.Println("Init gikk bra")
 	for{
 		state, master_adress := StateInit(broadcast_conn)	//bestemme funksjon
-		connections := make([]*net.TCPConn,10)
+		fmt.Println("State init gikk bra")
+		connections := make([]net.Conn,10)
 		nr_of_slaves := 0
 // 							master loop, n = number of slaves 					
  		for (state=="master"){
 //			if !CheckConnection(state, connections){
 //				break
 //			}
-			broadcast_conn.Write(ip)
-			connections[nr_of_slaves],err = MakeSlave(master_port)
+			fmt.Println("Heisen er en master")
+			broadcast_conn.Write([]byte(ip+"\x00"))
+			slave_listener, err := SlaveListener(master_port)
+			slave_listener.SetDeadline(time.Now().Add(2*time.Second))
+			connections[nr_of_slaves],err = slave_listener.Accept()
 			if err != nil{
+				fmt.Println("Finner ingen slaver")
+			}			
+			if err == nil{
 				nr_of_slaves = nr_of_slaves + 1
+				fmt.Println("inkrementerer variabel")
    			}
 			i := 0
 			buffer := make([]byte, 128)
 			for i < nr_of_slaves {
-				connections[i].Write("HAI there")
-				connection[i].Read(buffer)
-				fmt.printf(string(buffer))
+				connections[i].Write([]byte("HAI there"))
+				connections[i].Read(buffer)
+				fmt.Println(string(buffer))
 				i += 1
 			}
-			time.Sleep(0.1*time.Second)	
+			time.Sleep(100*time.Millisecond)	
 		}
 // 		"Videre: prossesering av data og sending av informasjon til slaver
 
 
  		if state == "slave" {
-			connections[nr_of_slaves],err := ConnectMaster(master_adress + master_port)
+			connections[0],err = ConnectMaster(master_adress + master_port)
 			if err != nil{
 				fmt.Println("Cannot connect to master: ", err);
  			
@@ -101,10 +109,12 @@ func NetworkModule(){
 //				break						
 //			}
 //			ReadElevatorStatus()
+			fmt.Println("Heisen er en slave")
 			buffer := make([]byte, 128)
-			connections[nr_of_slaves].Write("bai there")
+			connections[nr_of_slaves].Write([]byte("BAI there" + "\x00"))
 			connections[nr_of_slaves].Read(buffer)
-			fmt.printf(string(buffer))
+			fmt.Println(string(buffer))
+			time.Sleep(100*time.Millisecond)	
 //			HandleMastersOrders()
 		}
 	}
